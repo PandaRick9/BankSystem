01e7f57fc5b5b0d2f6b06e32eafc6da855e8e923
derfect90 <herettyr765@gmail.com>
2025-04-19 21:16:32 +0300

    create docker-compose and setup javaFX

--- src/main/java/by/teamwork/banksystem/HelloController.java
+++ src/main/java/by/teamwork/banksystem/HelloController.java
@@ -10,5 +10,21 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to Bank System!");
+        /*  Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+    SessionFactory sessionFactory =  configuration.buildSessionFactory();
+    Session session = sessionFactory.getCurrentSession();
+        try {
+                session.beginTransaction();
+
+                Person person = session.get(Person.class, 1);
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+
+        session.getTransaction().commit();
+        }finally {
+        sessionFactory.close();
+        }*/
     }
 }
+
+
