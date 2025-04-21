e2ad61e691fb980ae1b8d2f61282c4e7aac91189
derfect90 <herettyr765@gmail.com>
2025-04-22 00:47:06 +0300

    Create validation to registration, write data to db, finish singUpPage

--- src/main/java/by/teamwork/banksystem/StartApp.java
+++ src/main/java/by/teamwork/banksystem/StartApp.java
@@ -1,15 +1,12 @@
 package by.teamwork.banksystem;
 
-import by.teamwork.banksystem.models.Account;
-import by.teamwork.banksystem.models.Client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.flywaydb.core.Flyway;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+
+
 
 import java.io.IOException;
 
@@ -28,28 +25,6 @@ public class StartApp extends Application {
 
         flyway.migrate();
 
-        /*Configuration configuration = new Configuration().addAnnotatedClass(Client.class)
-                .addAnnotatedClass(Account.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        try {
-            session.beginTransaction();
-
-            Client client = new Client ();
-            client.setLastname("Ivanov");
-            client.setName("Ivan");
-            client.setPatronymic("Ivanovich");
-            client.setEmail("yes@gmail.com");
-            client.setPassword("12345");
-            client.setPhone("+375749209487");
-            client.setBirthday(LocalDateTime.now());
-            session.save(client);
-
-
-            session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
-        }*/
     }
 
     public static void main(String[] args) {
